1b3b78663e971b1cde35113e713261c4d2ecd6aa
joao melo <joao.melo.garrido@hotmail.com>
2019-05-11 17:10:40 +0100

    continuação do sistema de login nao acabado, REVER. Acrescentado janela da ajuda nao ligada as outras ainda. tentativa de ligar a base de dados.

--- ProjetoFinal_JoãoGarrido_06_EasyPolice/ProjetoFinal_JoãoGarrido_06_EasyPolice/Ajuda.Designer.cs
+++ ProjetoFinal_JoãoGarrido_06_EasyPolice/ProjetoFinal_JoãoGarrido_06_EasyPolice/Ajuda.Designer.cs
@@ -29,13 +29,24 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ajuda));
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(560, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Ajuda
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(772, 442);
+            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Ajuda";
@@ -46,5 +57,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
